d827d7751bfa3284dd399d1a817314c16c927e36
kylieyoo55 <60900990+kylieyoo55@users.noreply.github.com>
2021-01-08 14:12:55 +1300

    Complex States in Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,59 @@
 import React, { useState } from "react";
 
-
-
-
 function App() {
+  const [fullName, setFullName] = useState({
+    firstName: " ",
+    lastName: " "
+  });
 
-const [MouseOver, setMouseOver]=useState(false);
-const[name, setName]=useState(" ");
-const[heading,setHeading]=useState(" ")
+  const [heading, setHeading] = useState(" ");
+  const [isMouseOver, setMouseOver] = useState(false);
 
+  function HandleClick(e) {
+    setHeading(fullName.firstName + " " + fullName.lastName);
+    e.preventDefault();
+  }
 
-function HandleMouseOut(){
-  setMouseOver(false)
-  
-// setColor("black")
-}
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
 
-function HandleMouseOver(){
-  setMouseOver(true)
-}
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
 
-function HandleChange(e){
-  setName(e.target.value)
+  function HandleChange(e) {
+    const { value, name } = e.target;
 
-}
-
-
-function HandleClick(e){
-  setHeading(name);
-
-  e.preventDefault();
-}
+    setFullName((p) => {
+      if (name === "fName") {
+        return {
+          firstName: value,
+          lastName: p.lastName
+        };
+      } else if (name === "lName") {
+        return {
+          firstName: p.firstName,
+          lastName: value
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
       <h1>Hello {heading}</h1>
       <form onSubmit={HandleClick}>
-      <input
-       onChange={HandleChange}
-        type="text" 
-        placeholder="What's your name?"
-        // value={name}
-        />
-      <button 
-      type="submit"
-      style={{backgroundColor: MouseOver? "black": "white" }} 
-      onMouseOver={HandleMouseOver} 
-      onMouseOut={HandleMouseOut}
-
-      >Submit</button>
+        <input onChange={HandleChange} name="fName" placeholder="First Name" />
+        <input onChange={HandleChange} name="lName" placeholder="Last Name" />
+        <button
+          style={{ backgroundColor: isMouseOver ? "blue" : "white" }}
+          type="submit"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
