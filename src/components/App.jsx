cc9a71c6a18f928ba63590bbf123257f79b6a7c7
kylieyoo55 <60900990+kylieyoo55@users.noreply.github.com>
2021-01-08 16:06:16 +1300

    even more coplex task extended object inside object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,29 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = useState({
-    firstName: " ",
-    lastName: " "
+  const [contact, setContact] = useState({
+    fName: " ",
+    lName: " ",
+    email: " "
   });
 
-  const [heading, setHeading] = useState(" ");
-  const [isMouseOver, setMouseOver] = useState(false);
-
-  function HandleClick(e) {
-    setHeading(fullName.firstName + " " + fullName.lastName);
-    e.preventDefault();
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
-
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function HandleChange(e) {
+  function handleChange(e) {
     const { value, name } = e.target;
 
-    setFullName((p) => {
-      if (name === "fName") {
-        return {
-          firstName: value,
-          lastName: p.lastName
-        };
-      } else if (name === "lName") {
-        return {
-          firstName: p.firstName,
-          lastName: value
-        };
-      }
-    });
+    setContact((p) => ({ ...p, [name]: value }));
   }
 
   return (
     <div className="container">
-      <h1>Hello {heading}</h1>
-      <form onSubmit={HandleClick}>
-        <input onChange={HandleChange} name="fName" placeholder="First Name" />
-        <input onChange={HandleChange} name="lName" placeholder="Last Name" />
-        <button
-          style={{ backgroundColor: isMouseOver ? "blue" : "white" }}
-          type="submit"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          Submit
-        </button>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
+        <button>Submit</button>
       </form>
     </div>
   );
